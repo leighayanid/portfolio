f4046605d1584f4d5891a2198980ce867b6d4777
Leigh Dinaya <leighdinaya04@gmail.com>
2020-12-07 14:00:36 +0800

    fix fonts name

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import "~/assets/style/index.scss";
 
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from "~/layouts/Default.vue";
-import FiraSansMedium from "~/assets/fonts/FiraSans-Medium.woff2";
-import FiraSansRegular from "~/assets/fonts/FiraSans-Regular.woff2";
-import MerriweatherRegular from "~/assets/fonts/Merriweather-Regular.woff2";
+import FiraSansMedium from "~/assets/fonts/FiraSans-Medium.163b97d1.woff2";
+import FiraSansRegular from "~/assets/fonts/FiraSans-Regular.cfa5be9e.woff2";
+import MerriweatherRegular from "~/assets/fonts/Merriweather-Regular.e09790a3.woff2";
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function(Vue, { head }) {
