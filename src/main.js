b9d0d23b46d35d5e054b5b504040267eaed67b8f
Leigh Dinaya <leighdinaya04@gmail.com>
2020-12-07 17:34:56 +0800

    fix preload fonts

--- src/main.js
+++ src/main.js
@@ -3,17 +3,9 @@ import "~/assets/style/index.scss";
 
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from "~/layouts/Default.vue";
-import FiraSansMedium from "~/assets/fonts/FiraSans-Medium.woff2";
-import FiraSansRegular from "~/assets/fonts/FiraSans-Regular.woff2";
-import MerriweatherRegular from "~/assets/fonts/Merriweather-Regular.woff2";
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function(Vue, { head }) {
 	// Set default layout as a global component
 	Vue.component("Layout", DefaultLayout);
-	head.link.push([
-		{ rel: "preload", href: FiraSansMedium, as: "font" },
-		{ rel: "preload", href: FiraSansRegular, as: "font" },
-		{ rel: "preload", href: MerriweatherRegular, as: "font" },
-	]);
 }
