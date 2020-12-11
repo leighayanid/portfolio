29255e8dcda244683db9944be29243d028fe83b4
Leigh Dinaya <leighdinaya04@gmail.com>
2020-12-11 08:18:23 +0800

    switch toggle mode

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -5,8 +5,24 @@
     @click.prevent="toggleTheme"
     class="toggle-theme"
   >
+   
     <svg
-      v-if="darkTheme"
+      v-if="darkTheme""
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-moon"
+    >
+      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+    </svg>
+     <svg
+     v-else
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -27,21 +43,6 @@
       <line x1="21" y1="12" x2="23" y2="12"></line>
       <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
       <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-    </svg>
-    <svg
-      v-else
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="feather feather-moon"
-    >
-      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
     </svg>
   </button>
 </template>
